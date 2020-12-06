e7d0d4b418e964c9329528f072f530ca79bb47db
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2020-12-06 00:15:24 -0800

    fix build error somehow

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using power_classrooms.Models;
+using power_classroom.Models;
 
-namespace power_classrooms.Controllers
+namespace power_classroom.Controllers
 {
     public class HomeController : Controller
     {
