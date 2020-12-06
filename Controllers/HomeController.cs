d449678d41a64711e21a0651a9e51b86252efe0a
Taenerys <thuanh99@student.fdu.edu>
2020-12-05 22:57:56 -0800

    add case update page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using power_classrooms.Models;
+using System.Globalization;
 
 namespace power_classrooms.Controllers
 {
@@ -26,6 +27,19 @@ namespace power_classrooms.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult CaseUpdate()
+        {
+            var caseViewModel = new CaseViewModel()
+            {
+                Confirmed = 378343,
+                Deceased = 300478,
+                Recovered = 12130,
+                Date = DateTime.Now.ToString()
+            };
+
+            return View(caseViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
